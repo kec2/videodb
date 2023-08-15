3b6f28b61b99eac7d4252be7fb9109b6deff741c
klaus <klaus_edwin@hotmail.com>
2023-08-15 08:53:29 +0200

    revert db_server

--- config.sample.php
+++ config.sample.php
@@ -8,7 +8,7 @@
  */
 
 /* Database configuration */
-$config['db_server']    = 'mariadb';  // database server
+$config['db_server']    = 'localhost';  // database server
 $config['db_user']      = 'videodb';    // DB user for connection
 $config['db_password']  = '';           // password for above user
 $config['db_database']  = 'videodb';    // Database name
