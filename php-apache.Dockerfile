58ec6c2047d5a4fcb11afb25e3413ae4e4ec0775
kec2 <klaus_edwin@hotmail.com>
2025-05-23 14:25:47 +0200

    upgrade to php 8.4 and upgraded dependencies (#68)

--- php-apache.Dockerfile
+++ php-apache.Dockerfile
@@ -1,4 +1,4 @@
-FROM php:8.3-apache
+FROM php:8.4-apache
 
 # install GD dependencies
 RUN apt-get update && apt-get install -y \
--- test/index.php
+++ test/index.php
@@ -68,7 +68,7 @@ $config['debug'] = 0;
     <body>
 <?php
 
-$testClasses = findTestClasses('./test', $_REQUEST['test']);
+$testClasses = findTestClasses('./test', $_REQUEST['test'] || '');
 
 // 'loadedExtensions', 'extensions', 'notLoadedExtensions' is just to avoid PHP 7.4 warnings.
 $args = [];
@@ -109,7 +109,7 @@ $config['debug'] = $origDebug;
  * @param string    pattern If set then only test containing this pattern will be executed
  * @psalm-return array<string, string>
  */
-function findTestClasses(string $rootDir, string $pattern = null): array {
+function findTestClasses(string $rootDir, string $pattern = 'FooBar'): array {
     $result = array();
 
     $directory = new \RecursiveDirectoryIterator($rootDir);
